44ad0e1265e0f6379337c7176c5e37f7a1ea9ba8
Serhii_Y <dj.ser961@gmail.com>
2019-12-07 00:59:56 +0200

    Updates

---
+++ precache-manifest.b6e4ac49f3e4b8712187ecf08d6f86be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0096c187a98b7f777e6cd34836367b",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "4432c6d0ea4e6c726bbb",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.951a195d.chunk.css"
+  },
+  {
+    "revision": "35572e828275f31fb558",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.23af80c0.chunk.js"
+  },
+  {
+    "revision": "4432c6d0ea4e6c726bbb",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.a96a68e3.chunk.js"
+  },
+  {
+    "revision": "db10479c1573f6125dd1",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0eafb310.js"
+  }
+]);
